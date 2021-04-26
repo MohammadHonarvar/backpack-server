28f26fe038e4499e73d8f0a0b6336de18e021a54
Mohammad Honarvar <honarvar.info@gmail.com>
2021-04-27 02:45:16 +0430

    feat(sign-up): complete the process of the user signing up

--- src/modules/user/sign-up.js
+++ src/modules/user/sign-up.js
@@ -1,7 +1,71 @@
 import { debug } from '../../common/log.js';
 const log = debug('app/modules/user/sign-up');
 
-export const signUp = () => {
-  log('signUp done');
-  return 'New user created';
+import { hash } from 'bcrypt';
+import { mysqlConnection } from '../mysql-connection.js';
+import { validateEmail } from '../../common/validate-email.js';
+import { validatePasswordFormat } from '../../common/validate-password-format.js';
+
+export const signUp = async (optionList) => {
+  log('`signUp` got this `optionList`: %o', optionList);
+
+  if (!validateEmail(optionList.email)) {
+    throw {
+      ok: false,
+      errorCode: 104,
+      description: 'Invalid `email` format',
+    };
+  }
+
+  if (!validatePasswordFormat(optionList.password)) {
+    throw {
+      ok: false,
+      errorCode: 104,
+      description:
+        'Invalid `password` format, a password should contains at least 6 chars (uppercase, lowercase, number and special chars)',
+    };
+  }
+
+  let dbConnection = null;
+  try {
+    dbConnection = await mysqlConnection();
+  } catch (error) {
+    log(error);
+
+    throw {
+      ok: false,
+      errorCode: 105,
+      description: 'DB connection error',
+    };
+  }
+
+  const userSql = 'SELECT id FROM users WHERE email = ?';
+  let [rows, fields] = await dbConnection.execute(userSql, [optionList.email]);
+  log('result: %o', { rows, fields });
+
+  if (rows[0] != null) {
+    throw {
+      ok: false,
+      errorCode: 106,
+      description: 'Duplicate user email',
+    };
+  }
+
+  const hashedPassword = await hash(optionList.password, 9);
+  const insertUserSql = `INSERT INTO users (email, password) VALUES ('${optionList.email}', '${hashedPassword}')`;
+  [rows, fields] = await dbConnection.execute(insertUserSql);
+  log('Insert result: %o', { rows, fields });
+
+  if (!rows['insertId']) {
+    throw {
+      ok: false,
+      errorCode: 107,
+      description: 'Registering failed',
+    };
+  }
+
+  return {
+    ok: true,
+    description: 'The user registered',
+  };
 };
--- src/request-handlers/user/sign-up.js
+++ src/request-handlers/user/sign-up.js
@@ -1,8 +1,28 @@
-import { responseEnd } from '../../common/response-end.js';
-
 import { debug } from '../../common/log.js';
 const log = debug('app/request-handlers/user/sign-up');
 
-export const userSignUpHandler = (request, response) => {
-  return responseEnd(response, 200, { test2: 'sdfsdf' });
+import { bodyParser } from '../../common/body-parser.js';
+import { signUp } from '../../modules/user/sign-up.js';
+import { responseEnd } from '../../common/response-end.js';
+
+export const userSignUpHandler = async (request, response) => {
+  if (request.method !== 'POST') {
+    return responseEnd(response, 400, {
+      ok: false,
+      description: 'Invalid request',
+    });
+  }
+
+  const body = await bodyParser(request);
+  log('Body of request is: %o', body);
+  if (!(body.email != null && body.password != null)) {
+    throw {
+      ok: false,
+      errorCode: 103,
+      description: '`email` and `password` is required to sign up',
+    };
+  }
+
+  const signUpResult = await signUp(body);
+  return responseEnd(response, 200, signUpResult);
 };
