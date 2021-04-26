4a8c1a867be1142690e4956fcf3c43613dd5033c
Mohammad Honarvar <honarvar.info@gmail.com>
2021-04-27 03:43:41 +0430

    feat(login): complete the process of the user login

--- src/modules/user/login.js
+++ src/modules/user/login.js
@@ -1,7 +1,72 @@
 import { debug } from '../../common/log.js';
 const log = debug('app/modules/user/login');
 
-export const login = () => {
-  log('login done');
-  return 'New user created';
+import { compare } from 'bcrypt';
+import jsonwebtoken from 'jsonwebtoken';
+import { mysqlConnection } from '../mysql-connection.js';
+import { validateEmail } from '../../common/validate-email.js';
+import { validatePasswordFormat } from '../../common/validate-password-format.js';
+
+export const login = async (optionList) => {
+  log('`login` got this `optionList`: %o', optionList);
+
+  if (!validateEmail(optionList.email)) {
+    throw {
+      ok: false,
+      errorCode: 104,
+      description: 'Invalid `email` format',
+    };
+  }
+
+  if (!validatePasswordFormat(optionList.password)) {
+    throw {
+      ok: false,
+      errorCode: 104,
+      description:
+        'Invalid `password` format, a password should contains at least 6 chars (uppercase, lowercase, number and special chars)',
+    };
+  }
+
+  let dbConnection = null;
+  try {
+    dbConnection = await mysqlConnection();
+  } catch (error) {
+    log(error);
+    throw {
+      ok: false,
+      errorCode: 105,
+      description: 'DB connection error',
+    };
+  }
+
+  const userSql = 'SELECT * FROM users WHERE email = ?';
+  const [rows] = await dbConnection.execute(userSql, [optionList.email]);
+  log('result: %o', { rows });
+  dbConnection.end();
+
+  const user = rows[0];
+  if (user == null) {
+    throw {
+      type: 'UNAUTHORIZED',
+      description: 'Invalid user credentials',
+    };
+  }
+
+  const validPassword = await compare(optionList.password, user.password);
+  if (!validPassword) {
+    throw {
+      type: 'UNAUTHORIZED',
+      description: 'Invalid user credentials',
+    };
+  }
+
+  const userToken = jsonwebtoken.sign({ userId: user.id, email: user.email }, `${process.env.JWT_KEY}`);
+
+  return {
+    ok: true,
+    description: 'Successful login process',
+    data: {
+      token: userToken,
+    },
+  };
 };
--- src/request-handlers/user/login.js
+++ src/request-handlers/user/login.js
@@ -1,9 +1,28 @@
-import { responseEnd } from '../../common/response-end.js';
-
 import { debug } from '../../common/log.js';
 const log = debug('app/request-handlers/user/login');
 
-export const userLoginHandler = (request, response) => {
-  log('userLoginHandler is running...');
-  return responseEnd(response, 200, { test: 'adads' });
+import { bodyParser } from '../../common/body-parser.js';
+import { login } from '../../modules/user/login.js';
+import { responseEnd } from '../../common/response-end.js';
+
+export const userLoginHandler = async (request, response) => {
+  if (request.method !== 'POST') {
+    throw {
+      type: 'BAD_REQUEST',
+      description: 'Invalid request',
+    };
+  }
+
+  const body = await bodyParser(request);
+  log('Body of request is: %o', body);
+  if (!(body.email != null && body.password != null)) {
+    throw {
+      ok: false,
+      errorCode: 103,
+      description: '`email` and `password` is required to sign up',
+    };
+  }
+
+  const loginResult = await login(body);
+  return responseEnd(response, 200, loginResult);
 };
